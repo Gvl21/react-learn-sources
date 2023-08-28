302680bce4258826af7068a0e4a26c6ae9894772
Gvl21 <thornyk1@gmail.com>
2023-08-29 01:02:40 +0900

    #

--- profile/src/component/ProfileEditor.jsx
+++ profile/src/component/ProfileEditor.jsx
@@ -1,8 +1,41 @@
 import React, { useRef, useState } from 'react';
 
-function ProfileEditor({}) {
-  return;
-  <div></div>;
+function ProfileEditor({ onSave, user, userSet }) {
+  const onChangeHandler = (event) => {
+    userSet((user) => ({
+      ...user,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  //   const onClickHandler = () => {
+  //     onSave()
+  //   };
+
+
+  return (
+    <div>
+      <input
+        type='text'
+        id='nameId'
+        value={user.name}
+        onChange={onChangeHandler}
+      />
+      <input
+        type='text'
+        id='emailId'
+        value={user.email}
+        onChange={onChangeHandler}
+      />
+      <input
+        type='text'
+        id='ageId'
+        value={user.age}
+        onChange={onChangeHandler}
+      />
+      <button onClick={onSave}>눌러</button>
+    </div>
+  );
 }
 
 export default ProfileEditor;
